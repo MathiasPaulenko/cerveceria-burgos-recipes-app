52fc8d8bf937a69ebc26c16d1d8f06f47c774a2e
mathias <mathias.paulenko@gmail.com>
2026-06-05 15:45:43 +0800

    fix: correct vite base path to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/cerveceria-burgos-recipes/',
+  base: '/cerveceria-burgos-recipes-app/',
   plugins: [
     react(),
     VitePWA({
